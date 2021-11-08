5e3d32589b160f1b704ebe9a08e3601e33940579
Denilson Granados <denilsongranados4@gmail.com>
2021-11-07 22:31:18 -0500

    Implemented needed UI faceklift

--- week-05/src/main/java/learn/DWMH/UI/View.java
+++ week-05/src/main/java/learn/DWMH/UI/View.java
@@ -55,13 +55,10 @@ public class View {
             io.println("No reservations found.");
             return;
         }
-        for (Reservation reservation : reservations) {
-            io.printf("%s,%s,%s,%s,%s%n",
-                    reservation.getId(),
-                    reservation.getStartDate(),
-                    reservation.getEndDate(),
-                    reservation.getGuest().getGuestId(),
-                    reservation.getTotal());
+        int index=1;
+        io.printf("%-10s%-20s%-20s%-10s%-10s%n","Index","Start","End","Guest Id","Total");
+        for (Reservation res : reservations.stream().limit(25).collect(Collectors.toList())) {
+            io.printf("%-10s%-20s%-20s%-10s%-10s%n", index++, res.getStartDate(), res.getEndDate(),res.getGuest().getGuestId(),res.getTotal());
         }
     }
     public String getState(){
@@ -97,8 +94,9 @@ public class View {
             return null;
         }
         int index = 1;
+        io.printf("%-10s%-35s%-18s%-10s%n","Index","Email","First Name","Last Name");
         for (Guest guest : guests.stream().limit(25).collect(Collectors.toList())) {
-            io.printf("%s: %s %s%n", index++, guest.getFirstName(), guest.getLastName());
+            io.printf("%-10s%-35s%-18s%-10s%n", index++, guest.getEmail(),guest.getFirstName(), guest.getLastName());
         }
         index--;
 
@@ -116,13 +114,15 @@ public class View {
     }
 
     public Host chooseHost(List<Host> hosts) {
+
         if (hosts.size() == 0) {
             io.println("No hosts found");
             return null;
         }
         int index = 1;
+        io.printf("%-10s%-35s%-15s%-10s%-10s%n","Index","Email","Last Name","Week Rate","Weekend Rate");
         for (Host host : hosts.stream().limit(25).collect(Collectors.toList())) {
-            io.printf("%s: %s, %s%n", index++, host.getEmail(), host.getLastName());
+            io.printf("%-10s%-35s%-15s%-10s%-10s%n", index++, host.getEmail(), host.getLastName(),host.getStandardRate(),host.getWeekendRate());
         }
         index--;
 
@@ -144,8 +144,9 @@ public class View {
             return null;
         }
         int index = 1;
+        io.printf("%-10s%-10s%-10s%-10s%-10s%n","Index","Start","End","Guest Id","Total");
         for (Reservation res : reservations.stream().limit(25).collect(Collectors.toList())) {
-            io.printf("%s: %s, %s%n", index++, res.getStartDate(), res.getEndDate());
+            io.printf("%-10s%-10s%-10s%-10s%-10s%n", index++, res.getStartDate(), res.getEndDate(),res.getGuest().getGuestId(),res.getTotal());
         }
         index--;
 
